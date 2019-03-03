129de7d528eb2f71290bfc1440402503dd60c9de
kumar.yashkumar.yash1@gmail.com <kumar.yashkumar.yash1@gmail.com>
2019-03-03 22:14:39 +0530

    reverse a string

---
+++ reversestring.js
@@ -0,0 +1,24 @@
+function reverse(str){
+	//check input
+  if(!str || typeof str != 'string' || str.length < 2 ) return str;
+  
+  const backwards = [];
+  const totalItems = str.length - 1;
+  for(let i = totalItems; i >= 0; i--){
+    backwards.push(str[i]);
+  }
+  return backwards.join('');
+}
+
+//using inbulit function
+
+function reverse2(str){
+  //check for valid input
+  return str.split('').reverse().join('');
+}
+
+const reverse3 = str => [...str].reverse().join('');
+
+reverse('Timbits Hi')
+reverse('Timbits Hi')
+reverse3('Timbits Hi')
